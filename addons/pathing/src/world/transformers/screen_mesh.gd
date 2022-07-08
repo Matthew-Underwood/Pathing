0de0233ac921790a1099f49899c75e9d680d4c4c
Matthew Underwood <farban6@gmail.com>
2022-07-08 22:21:56 +0100

    Changes from using in waypoints

--- addons/pathing/src/world/transformers/screen_mesh.gd
+++ addons/pathing/src/world/transformers/screen_mesh.gd
@@ -7,7 +7,7 @@ func _init(mesh_picking : MUP_World_Mesh_Picking):
 	
 
 func transform(pos : Vector2) -> Vector3:
-	var result = _mesh_picking.pick(pos)
-	if !result.empty():
-		return result["position"]
-	return Vector3.ZERO
+    var result = _mesh_picking.pick(pos)
+    if !result.empty():
+        return result["position"]
+    return Vector3.ZERO
